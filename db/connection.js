0c673172b23dc2e786070071299956618bb48afc
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-04-28 00:08:03 -0700

    fixing heroku deployment bug

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // and .catch() for errors
 mongoose.Promise = Promise
 // set the uri for connecting to our local mongodb
-let mongoURI = ''
+let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.ATLAS_URI;
 } else {
