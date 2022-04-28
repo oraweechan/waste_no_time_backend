409f367ad1bad4cacf70c421865801240279cb59
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-04-28 09:39:06 -0700

    added error checks for routes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -5,28 +5,37 @@ const Event = require("../models/eventSchema");
 const User = require("../models/userSchema");
 const auth = require ('../middleware/auth.js');
 
-router.post("/", (req, res) => {
-  Event.create(req.body).then((event) => {
-    res.status(201).json({ status: 201, event: event });
-  });
-});
+//post event
+router.post("/", async (req, res) => {
+  try {
+    Event.create(req.body)
+    .then((event) => res.status(201).json({ status: 201, event: event }))
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+}});
 
+// get all events
 router.get("/", async (req, res) => {
   try {
     Event.find()
     .then((posts) => res.json(posts))
-  } catch(error) {
-    res.status(400).json("error: " + error.message);
+  } catch (error) {
+    res.status(400).json({error: error.message});
   } 
 });
-// router.get("/", auth, async (req, res) => {
-//   try {
-//     Event.find()
-//     .then((posts) => res.json(posts))
-//   } catch(error) {
-//     res.status(400).json("error: " + error.message);
-//   } 
-// });
+
+// get individual events
+router.get("/:id", async (req, res) => {
+  
+  try {
+    const { id } = req.params;
+    Event.findById(id)
+    .then((post) => res.json(post))
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  } 
+});
+
 
 //add event
 // router.route("/create/:id").post(auth, async (req, res) => {
@@ -58,11 +67,26 @@ router.route("/add/:id").put( async (req, res) => {
   }
 });
 
-
+//delete event
 router.delete("/:id", (req, res) => {
   Event.deleteOne({ _id: req.params.id }).then(() => {
     res.status(204).json();
   });
 });
+
+// update event
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const eventDoc = await Event.findById(id);
+    console.log(eventDoc)
+    const updateEvent = await eventDoc.updateOne(
+      req.body
+    )
+    const savedEvent = await eventDoc.save()
+    return res.json(savedEvent)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+}});
 
 module.exports = router;
