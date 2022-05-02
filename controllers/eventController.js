e1d2339ea84b0aae867ed5884f13609a80062fa0
danishm4557 <danishm4557@gmail.com>
2022-05-02 17:14:12 -0500

    added put route

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,41 +1,36 @@
-
 const express = require("express");
 const router = express.Router();
 const Event = require("../models/eventSchema");
 const User = require("../models/userSchema");
-const auth = require ('../middleware/auth.js');
+const auth = require("../middleware/auth.js");
 
 //post event
 router.post("/", async (req, res) => {
   try {
-    Event.create(req.body)
-    .then((event) => res.status(201).json({ status: 201, event: event }))
+    Event.create(req.body).then((event) => res.status(201).json({ status: 201, event: event }));
   } catch (error) {
     res.status(500).json({ error: error.message });
-}});
+  }
+});
 
 // get all events
 router.get("/", async (req, res) => {
   try {
-    Event.find()
-    .then((posts) => res.json(posts))
+    Event.find().then((posts) => res.json(posts));
   } catch (error) {
-    res.status(400).json({error: error.message});
-  } 
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // get individual events
 router.get("/:id", async (req, res) => {
-  
   try {
     const { id } = req.params;
-    Event.findById(id)
-    .then((post) => res.json(post))
+    Event.findById(id).then((post) => res.json(post));
   } catch (error) {
-    res.status(400).json({error: error.message});
-  } 
+    res.status(400).json({ error: error.message });
+  }
 });
-
 
 //add event
 // router.route("/create/:id").post(auth, async (req, res) => {
@@ -52,16 +47,14 @@ router.get("/:id", async (req, res) => {
 //     .catch((err) => res.status(400).json(err));
 // });
 
-router.route("/add/:id").put( async (req, res) => {
+router.route("/add/:id").put(async (req, res) => {
   try {
     const { email } = req.body;
-    const  eventId  = req.params.id;
-    const eventDoc = await Event.findById(eventId)
-    const addUserEvent = await eventDoc.updateOne(
-      { $addToSet:{ users: email}}
-    )
-    const savedEvent = await eventDoc.save()
-    return res.json(savedEvent)
+    const eventId = req.params.id;
+    const eventDoc = await Event.findById(eventId);
+    const addUserEvent = await eventDoc.updateOne({ $addToSet: { users: email } });
+    const savedEvent = await eventDoc.save();
+    return res.json(savedEvent);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -79,14 +72,13 @@ router.put("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const eventDoc = await Event.findById(id);
-    console.log(eventDoc)
-    const updateEvent = await eventDoc.updateOne(
-      req.body
-    )
-    const savedEvent = await eventDoc.save()
-    return res.json(savedEvent)
+    console.log(eventDoc);
+    const updateEvent = await eventDoc.updateOne(req.body);
+    const savedEvent = await updateEvent.save();
+    return res.json(savedEvent);
   } catch (error) {
     res.status(500).json({ error: error.message });
-}});
+  }
+});
 
 module.exports = router;
